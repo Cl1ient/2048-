7203cc58ebf2a6169282b3b1b0094df0f4aaa3a6
Erm5s <emre_sen@icloud.com>
2026-01-08 16:50:53 +0100

    Fonctions pour les gestion de déplcement et de fusion

--- game_process.c
+++ game_process.c
@@ -6,7 +6,7 @@ int pipe_anon[2];
 pid_t pid_display;
 
 
-void thread_wakeup(int sig) {}
+void thread_weup(int sig) {}
 
 // Apparition d'une tuile aléatoire (2 ou 4) sur une case vide 
 void add_tile() {
@@ -35,6 +35,76 @@ void add_tile() {
     }
 }
 
+// Gère la logique de déplacement ligne par ligne OU colonne par colonne
+void move_logic(Command cmd) {
+    int moved = 0;
+    int temp_line[4];
+
+    for (int i = 0; i < 4; i++) {
+        // "copie" du jeu actuel dans l'ordre souhaité
+        for (int j = 0; j < 4; j++) {
+            if (cmd == LEFT)  temp_line[j] = game.grid[i][j];
+            else if (cmd == RIGHT) temp_line[j] = game.grid[i][3-j]; // début par la fin de ligne
+            else if (cmd == UP)    temp_line[j] = game.grid[j][i];
+            else if (cmd == DOWN)  temp_line[j] = game.grid[3-j][i]; // début par le fin de colonne
+        }
+        
+        // déplacement et fusion de toute la ligne / colonne
+        if (process_line(temp_line))
+            moved = 1; 
+
+        // maj du jeu après déplacement et fusion
+        for (int j = 0; j < 4; j++) {
+            if (cmd == LEFT)  game.grid[i][j] = temp_line[j];
+            else if (cmd == RIGHT) game.grid[i][3-j] = temp_line[j];
+            else if (cmd == UP)    game.grid[j][i] = temp_line[j];
+            else if (cmd == DOWN)  game.grid[3-j][i] = temp_line[j];
+        }
+    }
+
+    if (moved) add_tile();
+}
+
+// Déplace et fusionne n'importe quelle ligne/colonne comme un mouvement vers la gauche
+int process_line(int line[4]) {
+    int changed = 0;
+    int result[4] = {0, 0, 0, 0};
+    int pos = 0;
+
+    // glissement des tuiles jusqu'à rencontrer un bord ou une autre tuile
+    for (int i = 0; i < 4; i++) {
+        if (line[i] != 0) 
+            result[pos++] = line[i];
+    }
+
+    // fusion de deux tuiles adjancentes de même valeur
+    for (int i = 0; i < 3; i++) {
+        if (result[i] != 0 && result[i] == result[i+1]) {
+            result[i] *= 2;
+            game.score += result[i];
+            result[i+1] = 0;
+            changed = 1;
+        }
+    }
+
+    // re-glissment des tuiles
+    pos = 0;
+    int final_line[4] = {0};
+    for (int i = 0; i < 4; i++) {
+        if (result[i] != 0) 
+            final_line[pos++] = result[i];
+    }
+
+    // maj de la ligne
+    for (int i = 0; i < 4; i++) {
+        if (line[i] != final_line[i]) {
+            line[i] = final_line[i];
+            changed = 1;
+        }
+    }
+    return changed;
+}
+
 void* thread_move_score(void* arg) {}
 
 void* thread_goal(void* arg) {}
@@ -45,10 +115,10 @@ int main() {
     add_tile();
 
     pipe(pipe_anon);
-    pid_display = fork();
+    pid_display = for();
 
     t_main = pthread_self();
-    signal(SIGUSR1, thread_wakeup);
+    signal(SIGUSR1, thread_waaeup);
 
     pthread_create(&t_move, NULL, thread_move_score, NULL);
     pthread_create(&t_goal, NULL, thread_goal, NULL);
